fddd6154c196a5d22b62e41984ce35fc90094df4
sajibkhandev <sajibkhan.mern@gmail.com>
2024-12-29 17:31:29 +0600

    project start

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize:{
+        "40":"40px"
+      }
+    },
+  },
+  plugins: [],
+}
+

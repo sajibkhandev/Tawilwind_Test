6ffe8697cbe364a113eeefdf913f1f21a6e22816
sajibkhandev <sajibkhan.mern@gmail.com>
2024-12-29 17:40:21 +0600

    more card added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <>
+     <div className='w-[300px]  bg-blue-300 rounded-xl py-10 px-5 mt-10 ml-10'>
+
+<h1>Thsi is sajib khan</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laboriosam veritatis nam nostrum fugit velit dicta error? Corporis accusamus nulla aspernatur</p>
+<button className='bg-purple-500 px-10 py-4 rounded-xl mt-6'>Shoe more</button>
+
+</div>
+<div className='w-[300px]  bg-blue-300 rounded-xl py-10 px-5 mt-10 ml-10'>
+
+<h1>Thsi is sajib khan</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laboriosam veritatis nam nostrum fugit velit dicta error? Corporis accusamus nulla aspernatur</p>
+<button className='bg-purple-500 px-10 py-4 rounded-xl mt-6'>Shoe more</button>
+
+</div>
+<div className='w-[300px]  bg-blue-300 rounded-xl py-10 px-5 mt-10 ml-10'>
+
+<h1>Thsi is sajib khan</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laboriosam veritatis nam nostrum fugit velit dicta error? Corporis accusamus nulla aspernatur</p>
+<button className='bg-purple-500 px-10 py-4 rounded-xl mt-6'>Shoe more</button>
+
+</div>
       
-      <div className='w-[300px]  bg-blue-300 rounded-xl py-10 px-5 mt-10 ml-10'>
-
-        <h1>Thsi is sajib khan</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laboriosam veritatis nam nostrum fugit velit dicta error? Corporis accusamus nulla aspernatur</p>
-        <button className='bg-purple-500 px-10 py-4 rounded-xl mt-6'>Shoe more</button>
-
-      </div>
-    </div>
+     
+    </>
   )
 }
 
